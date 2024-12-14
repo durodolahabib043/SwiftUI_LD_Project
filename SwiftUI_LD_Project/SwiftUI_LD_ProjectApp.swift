d24652d06dc4e64d1fa79bcdea9b9f9e94c6752d
durodolahabib043 <durodolahabib043@gmail.com>
2024-12-14 13:17:41 -0500

    first commit

---
+++ SwiftUI_LD_Project/SwiftUI_LD_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LD_ProjectApp.swift
+//  SwiftUI_LD_Project
+//
+//  Created by Habib Durodola on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LD_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
