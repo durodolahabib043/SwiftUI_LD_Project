baf7f57e68bee28ed9572e38e46bd48d0fda1577
durodolahabib043 <durodolahabib043@gmail.com>
2024-12-15 09:08:56 -0500

    load only once

--- SwiftUI_LD_Project/Presentation/ContentView.swift
+++ SwiftUI_LD_Project/Presentation/ContentView.swift
@@ -10,49 +10,53 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject var contentViewModel = ContentViewModel()
-
-      private let adaptiveColumn = [
-          GridItem(.adaptive(minimum: 150))
-      ]
+    @State private var isFirstLoad = true
+    
+    private let adaptiveColumn = [
+        GridItem(.adaptive(minimum: 150))
+    ]
     
     var body: some View {
         
         NavigationStack {
             ScrollView{
-                  LazyVGrid(columns: adaptiveColumn, spacing: 20) {
-                      ForEach(contentViewModel.entry, id: \.id) { entry in
+                LazyVGrid(columns: adaptiveColumn, spacing: 20) {
+                    ForEach(contentViewModel.entry, id: \.id) { entry in
                         
-                          NavigationLink(destination: ContentViewDetails(entry: entry)) {
-                                               VStack(alignment: .center) {
-                                                   AsyncImage(url: URL(string: entry.image)) { image in
-                                                    image.resizable()
-                                                           .frame(height: 150)
-                                                   } placeholder: {
-                                                    ProgressView()
-                                                   }
-                                                   .frame(maxWidth: .infinity)
-                                                   Text(entry.name)
-                                                   Text(entry.code)
-                                                   Text(entry.price)
-                                                   Spacer()
-                                               }.foregroundColor(.black)
-                                               .border(.gray)
-                                               .frame(maxWidth: .infinity)
-                                           }
-                          
-                      }
-                  }
-                  
-              }
-              .padding()
-              .onAppear {
-                  Task {
-                      await contentViewModel.fetchClientCart()
-                  }
-              }
-              .navigationTitle("Product")
+                        NavigationLink(destination: ContentViewDetails(entry: entry)) {
+                            VStack(alignment: .center) {
+                                AsyncImage(url: URL(string: entry.image)) { image in
+                                    image.resizable()
+                                        .frame(height: 150)
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(maxWidth: .infinity)
+                                Text(entry.name)
+                                Text(entry.code)
+                                Text(entry.price)
+                                Spacer()
+                            }.foregroundColor(.black)
+                                .border(.gray)
+                                .frame(maxWidth: .infinity)
+                        }
+                        
+                    }
+                }
+                
+            }
+            .padding()
+            .task {
+                if isFirstLoad {
+                    Task {
+                        await contentViewModel.fetchClientCart()
+                    }
+                    isFirstLoad = false
+                }
+            }
+            .navigationTitle("Product")
         }
-
+        
     }
 }
 
